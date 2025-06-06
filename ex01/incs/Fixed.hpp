0a940d4f80608bd840a1338d2744b4dbe21b1ea1
Guillaume Ebersohl <guigui1991@live.fr>
2025-06-06 20:58:48 +0200

    feat: ex01 setup, nothing done yet

---
+++ ex01/incs/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class	Fixed
+{
+	private:
+		static const int	_fractionalBits;
+		int					_fixedPointValue;
+
+		public:
+			Fixed(void);
+			Fixed(Fixed& classCopyName);
+			Fixed(const int number);
+			Fixed(const float number);
+
+			~Fixed(void);
+
+			Fixed& operator=(Fixed& classCopyName);
+
+			int		getRawBits(void) const;
+			void	setRawBits(int const raw);
+};
+
+#endif
